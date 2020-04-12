11b7eedd3242e0dd6aaefc185abcdb6fe8b38df7
daniela2001-png <dmbmora@outlook.com>
2020-04-12 11:29:26 -0500

    mini_duel

---
+++ challenges.py
@@ -0,0 +1,27 @@
+#challenges with python_3
+# Write your square_root function here:
+def square_root(num):
+  raiz = num ** (1/2)
+  return raiz
+# Uncomment these function calls to test your square_root function:
+check = square_root(4)
+print(check)
+print(square_root(16))
+# should print 4
+print(square_root(100))
+# should print 10
+
+#other function
+
+# Write your tenth_power function here:
+def tenth_power(num):
+  return num ** 10
+# Uncomment these function calls to test your tenth_power function:
+comprobar = tenth_power(2)
+print(comprobar)
+print(tenth_power(1))
+# 1 to the 10th power is 1
+print(tenth_power(0))
+# 0 to the 10th power is 0
+print(tenth_power(2))
+# 2 to the 10th power is 1024
